75f3d1035556dc90bdfa93ca14dfb7044bd06292
jacobhogrefe <jhogrefe03@gmail.com>
2024-01-20 18:39:35 -0500

    frames now change colors based on if they are constant or not, and change back after a certain number of milliseconds if not constant

--- LaunchHelper.py
+++ LaunchHelper.py
@@ -5,20 +5,30 @@ import customtkinter as ctk
 import random
 
 class IndicatorFrame(ctk.CTkFrame):
-    def __init__(self, root, settings):
+    def __init__(self, root, settings, look_duration_time):
         super().__init__(root)
+        
+        # set the properties of the frame
         self.active = settings[0]
-        self.configure(fg_color="white")
-        self.bind("<Button-1>", self.change_color)
+        self.constant = settings[2]
+        self.duration = look_duration_time
+        self.configure(fg_color="white" if self.active else "transparent")
+        
+        # testing code
+        if self.active:
+            self.bind("<Button-1>", self.change_color)
     
     def change_color(self, event):
-        self.configure(fg_color="#{:06x}".format(random.randint(0, 0xFFFFFF))) 
+        if self.constant:
+            if self.cget("fg_color") == "white":
+                self.configure(fg_color="green")
+            else:
+                self.configure(fg_color="white")
+        else:
+            self.configure(fg_color="green")
+            self.after(self.duration, lambda: self.configure(fg_color="white")) 
         
 '''
-class to make a frame with a color
-be able to change the color of that frame
-make it so once it's clicked, it changes color
-
 function to start a camera feed
 
 function to end a camera feed
