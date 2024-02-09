a1ca15482d34cc695a216cab833e8f7f600d20c6
jacobhogrefe <jhogrefe03@gmail.com>
2024-02-09 12:55:30 -0500

    added hardware integration through the board and digitalio modules. added error checking if the FT232H board is disconnected (will be changed to hopefully not completely break the app)

--- LaunchHelper.py
+++ LaunchHelper.py
@@ -7,9 +7,18 @@ class IndicatorFrame(ctk.CTkFrame):
     def __init__(self, root, settings):
         super().__init__(root)
         
+        # loads in modules once reaching this screen
+        self._load_modules()
+        
         # set the properties of the frame
         self._active = settings[0]
-        self._pin = settings[1]
+        if self._active:
+            try:
+                self._pin = digitalio.DigitalInOut(getattr(board, settings[1]))
+                self._pin.direction = digitalio.Direction.OUTPUT
+            except Exception as e:
+                error = ctk.CTkLabel(self, text=f"Error: {e}. Please restart application", font=ctk.CTkFont(size=15), wraplength=200, text_color='black')
+                error.place(relx=.5, rely=.5, anchor=ctk.CENTER)
         self._constant = settings[2]
         self._out_duration = settings[3]
         
@@ -25,23 +34,32 @@ class IndicatorFrame(ctk.CTkFrame):
         return self._active
     
     @property
-    def pin(self) -> str:
-        return self._pin
-    
-    @property
     def constant(self) -> bool:
         return self._constant
     
     # change the color of the frame depending on what the settings are
     def change_color(self, event):
-        if self._constant and self._active:
-            if self.cget("fg_color") == "white":
-                self.configure(fg_color="green")
+        if self.active:
+            if self._constant:
+                if self.cget("fg_color") == "white":
+                    self.configure(fg_color="green")
+                    self._pin.value = True
+                else:
+                    self.configure(fg_color="white")
+                    self._pin.value = False
             else:
-                self.configure(fg_color="white")
-        elif self._active:
-            self.configure(fg_color="green")
-            self.after(self._out_duration, lambda: self.configure(fg_color="white"))
+                self._pin.value = True
+                self.configure(fg_color="green")
+                self.after(self._out_duration, lambda: self._reset_pin())
+            
+    def _reset_pin(self):
+        self.configure(fg_color="white")
+        self._pin.value = False
+    
+    def _load_modules(self):
+        global board, digitalio
+        import board
+        import digitalio
         
 '''
 function to start a camera feed
