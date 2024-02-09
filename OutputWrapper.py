b51348090b027177e1083d971e4622c776b31132
jacobhogrefe <jhogrefe03@gmail.com>
2024-02-09 13:00:16 -0500

    modified for testing, will most likely be deleted at some point

--- OutputWrapper.py
+++ OutputWrapper.py
@@ -1,9 +1,14 @@
 import os
 import platform
+
 # sets environment variables before loading board module to prevent errors
 os.environ["BLINKA_FT232H"] = "1"
 if platform.system() == "Darwin":
     os.environ["DYLD_LIBRARY_PATH"] = "/usr/local/Cellar/libusb/1.0.26/lib"
 elif platform.system() == "Windows":
     print("idk i'm on a mac")
+
+import digitalio
 import board
+
+print(dir(board))
