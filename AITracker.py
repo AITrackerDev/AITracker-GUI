80bba37389d419f218a80d7d92d929cb6576c825
jacobhogrefe <jhogrefe03@gmail.com>
2024-02-09 12:59:29 -0500

    opens the camera once the 'Open Camera' button is pressed

--- AITracker.py
+++ AITracker.py
@@ -2,12 +2,17 @@
 This file contains all of the necessary code to start the application and run through all of the necessary startup requirements.
 '''
 
-import cv2
-import tkinter as tk
-from tkinter import Canvas, PhotoImage
-import customtkinter as ctk
-from Screens import MainScreen
+import os
+import platform
 
+# sets environment variables before loading board module to prevent errors
+os.environ["BLINKA_FT232H"] = "1"
+if platform.system() == "Darwin":
+    os.environ["DYLD_LIBRARY_PATH"] = "/usr/local/Cellar/libusb/1.0.26/lib"
+elif platform.system() == "Windows":
+    print("idk i'm on a mac")
+
+import ctypes
 import cv2
 import tkinter as tk
 from tkinter import Canvas, PhotoImage
@@ -31,15 +36,16 @@ class AITracker(ctk.CTk):
         self.show_screen(MainScreen)
 
         # open the webcam
-        self.cap = cv2.VideoCapture(0)
+        self.cap = None
 
         # create a button to open the webcam feed in a separate window
-        self.open_camera_button = tk.Button(self, text="Open Camera", command=self.open_camera_window)
+        self.open_camera_button = ctk.CTkButton(self, text="Open Camera", command=self.open_camera_window)
         self.open_camera_button.pack(side=tk.TOP)
 
     def open_camera_window(self):
         # Create a new Toplevel window for the camera feed
-        camera_window = tk.Toplevel(self)
+        self.cap = cv2.VideoCapture(0)
+        camera_window = ctk.CTkToplevel(self)
         camera_window.title("Camera Feed")
 
         # Create a canvas to display the webcam feed in the new window
@@ -53,6 +59,8 @@ class AITracker(ctk.CTk):
         ret, frame = self.cap.read()
 
         if ret:
+            # flip image
+            frame = cv2.flip(frame, 1)
             # Convert BGR image to RGB
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -100,49 +108,8 @@ class AITracker(ctk.CTk):
         self.cap.release()
 
 if __name__ == "__main__":
+    # fixes the GUI being bugged in windows
+    if platform.system() == "Windows":
+        ctypes.windll.shcore.SetProcessDpiAwareness(2)
     app = AITracker()
     app.mainloop()
-
-
-# import customtkinter as ctk
-# import platform
-# from Screens import MainScreen
-# import ctypes
-# import cv2
-#
-# class AITracker(ctk.CTk):
-#     def __init__(self):
-#         super().__init__()
-#         self.current_screen = None
-#
-#         # window setup
-#         self.title("aiTracker")
-#         ctk.set_appearance_mode("dark")
-#         ctk.set_default_color_theme("blue")
-#         self.geometry("1080x720")
-#         self.minsize(1080, 720)
-#         self.attributes('-topmost', 1)
-#         self.update()
-#         self.attributes('-topmost', 0)
-#         self.wm_attributes("-fullscreen", True)
-#         self.show_screen(MainScreen)
-#
-#     def show_screen(self, screen_class):
-#         # destroy current screen if exists
-#         if self.current_screen:
-#             self.current_screen.destroy()
-#
-#         # create and display the requested screen
-#         self.current_screen = screen_class(self, self.show_screen)
-#         self.current_screen.pack()
-#
-#
-#
-# if __name__ == '__main__':
-#     # fixes the GUI being bugged in windows
-#     if platform.system() == "Windows":
-#         ctypes.windll.shcore.SetProcessDpiAwareness(2)
-#
-#     # starts app
-#     app = AITracker()
-#     app.mainloop()
